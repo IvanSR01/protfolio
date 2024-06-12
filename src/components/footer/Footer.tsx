7144c5a7e891bfbfcf5fcc04eb4efa0e7aeacae4
Ivan Nelson <ivan.sryvkin@bk.ru>
2024-06-12 12:25:48 +0300

    upd

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,9 +4,9 @@ const Footer: FC = () => {
 	return (
 		<footer className={styles.footer}>
 			<div className={styles.items}>
-				<a>GitHub</a>
-				<a>Habr</a>
-				<a href=''>Telegram</a>
+				<a href='https://github.com/IvanSR01'>GitHub</a>
+				<a href='https://freelance.habr.com/freelancers/ivan-nelson'>Habr</a>
+				<a href='https://t.me/nelsonivanz'>Telegram</a>
 			</div>
 			<p>© 2024 all rights reserved.</p>
 		</footer>
